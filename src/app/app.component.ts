38723419e1b69e4e809961504db9319e7df30ac2
ronald <ronaldabellano@gmail.com>
2020-07-15 19:41:55 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mrapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'movie-rental-app';
+}
